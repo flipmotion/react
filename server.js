478fbfb2fb22b34bf7b102d0e2febe25ca8c5e27
flipmotion <flipmotions@gmail.com>
2016-03-25 14:05:03 +0300

    ract

--- server.js
+++ server.js
@@ -1,68 +1,23 @@
-// Babel ES6/JSX Compiler stage 8
-require('babel-register');
+var webpack = require('webpack')
+var webpackDevMiddleware = require('webpack-dev-middleware')
+var webpackHotMiddleware = require('webpack-hot-middleware')
+var config = require('./webpack.config')
 
-var swig  = require('swig');
-var React = require('react');
-var ReactDOM = require('react-dom/server');
-var Router = require('react-router');
-var routes = require('./app/routes');
+var app = new (require('express'))()
+var port = 3000
 
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var bodyParser = require('body-parser');
+var compiler = webpack(config)
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
+app.use(webpackHotMiddleware(compiler))
 
-var app = express();
-/*
-var mongoose = require('mongoose');
-var Character = require('./models/character');
-*/
-var server = require('http').createServer(app);
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + '/index.html')
+})
 
-var io = require('socket.io')(server);
-var onlineUsers = 0;
-
-app.set('port', process.env.PORT || 3000);
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(function(req, res) {
-	Router.match({ routes: routes.default, location: req.url }, function(err, redirectLocation, renderProps) {
-		if (err) {
-			res.status(500).send(err.message)
-		} else if (redirectLocation) {
-			res.status(302).redirect(redirectLocation.pathname + redirectLocation.search)
-		} else if (renderProps) {
-			var html = ReactDOM.renderToString(React.createElement(Router.RoutingContext, renderProps));
-			var page = swig.renderFile('views/index.html', { html: html });
-			res.status(200).send(page);
-		} else {
-			res.status(404).send('Page Not Found')
-		}
-	});
-});
-
-/**
- * Socket.io stuff.
- */
-/*mongoose.connect(config.database);
-mongoose.connection.on('error', function() {
-  console.info('Error: Could not connect to MongoDB. Did you forget to run `mongod`?');
-});*/
-
-io.sockets.on('connection', function(socket) {
-  onlineUsers++;
-
-  io.sockets.emit('onlineUsers', { onlineUsers: onlineUsers });
-
-  socket.on('disconnect', function() {
-    onlineUsers--;
-    io.sockets.emit('onlineUsers', { onlineUsers: onlineUsers });
-  });
-});
-
-server.listen(app.get('port'), function() {
-  console.log('Express server listening on port ' + app.get('port'));
-});
+app.listen(port, function(error) {
+  if (error) {
+    console.error(error)
+  } else {
+    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+  }
+})
