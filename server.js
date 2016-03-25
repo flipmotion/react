28b6c5cf7f97a3984c0b5500b1152d693a98812c
flipmotion <flipmotions@gmail.com>
2016-03-25 14:29:53 +0300

    up

--- server.js
+++ server.js
@@ -1,23 +1,8 @@
-var webpack = require('webpack')
-var webpackDevMiddleware = require('webpack-dev-middleware')
-var webpackHotMiddleware = require('webpack-hot-middleware')
-var config = require('./webpack.config')
+var http = require('http');
 
-var app = new (require('express'))()
-var port = 3000
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end('Hello Http');
+});
 
-var compiler = webpack(config)
-app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
-app.use(webpackHotMiddleware(compiler))
-
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + '/index.html')
-})
-
-app.listen(port, function(error) {
-  if (error) {
-    console.error(error)
-  } else {
-    console.info("==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
-  }
-})
+server.listen(8080);
